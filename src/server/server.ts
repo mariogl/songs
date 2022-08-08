2681b771d2c43cb2e1a1b7fabcd74ec775a0d5a0
Mario González <mario@mariogl.com>
2022-08-08 20:18:04 +0200

    Added sonar

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { generalError, notFoundError } from "./middlewares/errors";
 import bandsRouter from "./routers/bands";
 import songsRouter from "./routers/songs";
 
+const allowedOrigins = [
+  process.env.ALLOWED_ORIGIN_LOCAL,
+  process.env.ALLOWED_ORIGIN_PRODUCTION,
+];
+
 const app = express();
 
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 app.use("/bands", bandsRouter);
